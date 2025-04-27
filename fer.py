5f39a4ba82bf05acf3540483e87fb2282620f060
IlginVural <vuralilgin05@gmail.com>
2025-04-27 19:32:12 +0300

    classNames redeclared

--- fer.py
+++ fer.py
@@ -3,18 +3,21 @@ import pandas as pd
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
-# Load the dataset
-data = pd.read_csv('fer2013.csv')
+class FER2013DataLoader:
+    def __init__(self, filepath):
+        self.filepath = filepath
 
-# Preprocess the data
-X = []
-y = []
-for index, row in data.iterrows():
-    X.append(np.array(row['pixels'].split(), dtype='float32').reshape(48, 48, 1))
-    y.append(row['emotion'])
+    def load_data(self):
+        data = pd.read_csv(self.filepath)
 
-X = np.array(X) / 255.0  # Normalize pixel values
-y = to_categorical(np.array(y), num_classes=7)
+        X = []
+        y = []
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        for index, row in data.iterrows():
+            X.append(np.array(row['pixels'].split(), dtype='float32').reshape(48, 48, 1))
+            y.append(row['emotion'])
+
+        X = np.array(X) / 255.0
+        y = to_categorical(np.array(y), num_classes=7)
+
+        return train_test_split(X, y, test_size=0.2, random_state=42)
